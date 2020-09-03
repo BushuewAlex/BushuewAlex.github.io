0a866998f1c296e6a8df7d7b05623252a230ed4a
BushuewAlex <69979677+BushuewAlex@users.noreply.github.com>
2020-09-03 20:42:59 +0300

    Parf

--- Metropolitan perfumer/js/main.js
+++ Metropolitan perfumer/js/main.js
@@ -17,6 +17,7 @@ window.onclick = function (Event) {
 }
 
 
+
 var modal__first = document.getElementById('myModal__first');
 var btn__first = document.getElementById('myBtn__first');
 var span__first = document.getElementsByClassName("close__first")[0];
@@ -53,4 +54,39 @@ window.onclick = function (Event) {
     if (Event.target == modal__second) {
         modal__second.style.display = "none";
     }
+}
+
+
+
+var modal__thirth = document.getElementById('myModal__thirth');
+var btn__thirth = document.getElementById('myBtn__thirth');
+var span__thirth = document.getElementsByClassName("close__thirth")[0];
+
+btn__thirth.onclick = function () {
+    modal__thirth.style.display = "block";
+}
+
+span__thirth.onclick = function () {
+    modal__thirth.style.display = "none";
+}
+
+window.onclick = function (Event) {
+    if (Event.target == modal__thirth) {
+        modal__thirth.style.display = "none";
+    }
+}
+
+
+
+let coll = document.getElementsByClassName('collapsible');
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+        let content__collapsible = this.nextElementSibling();
+        if (content__collapsible.style.maxHeight) {
+            content__collapsible.style.maxHeight = null;
+        } else {
+            content__collapsible.style.maxHeight = content__collapsible.scrollHeight + "px";
+        }
+    })
 }
